5cbc4cd8f0b4c06c5903810b963ce54d04511cb2
shrutiban <34169935+shrutiban@users.noreply.github.com>
2021-04-25 03:30:22 +0530

    Update app-routing.module.ts

--- FrontEnd/app-routing.module.ts
+++ FrontEnd/app-routing.module.ts
@@ -4,11 +4,23 @@ import { DemographicFormComponent } from './demographic-form/demographic-form.co
 import { HomePageComponent } from './home-page/home-page.component';
 import { LoginPageComponent } from './login-page/login-page.component'
 import { NavbarComponent } from './navbar/navbar.component';
+import { RegisterComponent } from './register/register.component';
+import { ProfileComponent } from './profile/profile.component';
+
+
+// it will be imported when role based components will be created
+// import { BoardUserComponent } from './board-user/board-user.component';
+// import { BoardModeratorComponent } from './board-moderator/board-moderator.component';
+// import { BoardAdminComponent } from './board-admin/board-admin.component';
+
+
 const routes: Routes = [
   {path:'',component:HomePageComponent},
   {path:'login',component:LoginPageComponent},
-  {path:'demographicForm',component:DemographicFormComponent}
-
+  {path:'demographicForm',component:DemographicFormComponent},
+  
+  { path: 'register', component: RegisterComponent },
+  { path: 'profile', component: ProfileComponent },
 ];
 
 @NgModule({
